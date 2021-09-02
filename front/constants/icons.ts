7588b007a65a41d62bd234f3f7c6ca23fccc201d
lee KR <benden4777@gmail.com>
2021-09-03 00:17:05 +0900

    하단 네비게이션 기본 커스텀 ing

--- front/constants/icons.ts
+++ front/constants/icons.ts
@@ -1,3 +1,5 @@
+const home = require("../assets/icons/ico_home.png")
+const search = require("../assets/icons/ico_search.png")
 const bookmarkLine = require("../assets/icons/ico_bookmark--line.png")
 const bookmarkNoLine = require("../assets/icons/ico_bookmark--no-line.png")
 const checkedBookmark = require("../assets/icons/ico_bookmark--cheked.png")
@@ -6,6 +8,8 @@ const checkedBookmark = require("../assets/icons/ico_bookmark--cheked.png")
 
 
 export default {
+    home,
+    search,
     bookmarkLine,
     bookmarkNoLine,
     checkedBookmark
--- front/navigator/AppNavigator.tsx
+++ front/navigator/AppNavigator.tsx
@@ -3,6 +3,8 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import HomeNavigator, { HomeNavigatorParams } from './HomeNavigator'
 import SearchNavigator, { SearchNavigatorParams } from './SearchNavigator'
 import { NavigatorScreenParams } from '@react-navigation/native'
+import {COLORS, icons} from '../constants'
+import TabIcons from '../components/TabIcons'
 
 export type AppNavigatorParams = {
 	Home: NavigatorScreenParams<HomeNavigatorParams>
@@ -13,9 +15,41 @@ const Tab = createBottomTabNavigator<AppNavigatorParams>()
 
 function AppNavigator() {
 	return (
-		<Tab.Navigator>
-			<Tab.Screen name="Home" component={HomeNavigator} />
-			<Tab.Screen name="Search" component={SearchNavigator} />
+		<Tab.Navigator
+			tabBarOptions={{
+				showLabel: false,
+				style: {
+					position: 'absolute',
+					bottom: 0,
+					left: 0,
+					right: 0,
+					elevation: 0,
+					backgroundColor: COLORS.white,
+					borderTopColor: COLORS.transparent,
+					height: 60
+				}
+			}}
+		>
+			<Tab.Screen 
+				name="Home"
+				component={HomeNavigator} 
+				options={{
+					tabBarIcon: ({ focused }) => <TabIcons 
+						focused={focused}
+						icon={icons.home}
+					/>
+				}}
+			/>
+			<Tab.Screen 
+				name="Search"
+				component={SearchNavigator}
+				options={{
+					tabBarIcon: ({ focused }) => <TabIcons 
+						focused={focused}
+						icon={icons.search}
+					/>
+				}}
+			/>
 		</Tab.Navigator>
 	)
 }
