e27acb11c5932b5f8bc8458e447c779c329f2d88
kimminsang <kimminsang713@gmail.com>
2021-09-26 23:54:07 +0900

    File upload Setting add

---
+++ backend/docker/file.Dockerfile
@@ -0,0 +1,6 @@
+FROM openjdk:16-jdk-alpine
+EXPOSE 8083
+RUN ./gradlew bootJar; mkdir -p /app/logs/
+WORKDIR /app
+COPY ../module-main/build/libs/module-main-0.0.1-SNAPSHOT.jar app.jar
+ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-Dspring.profiles.active=prod", "-jar", "app.jar"]
---
+++ backend/module-file/src/main/java/com/cookking/configs/WebConfiguration.java
@@ -0,0 +1,18 @@
+package com.cookking.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by minsang on 2021/09/26.
+ */
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
+    }
+}
