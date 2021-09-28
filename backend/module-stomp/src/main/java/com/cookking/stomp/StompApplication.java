f5df46622953f31c8fa95ed135a673bd36874059
sungjin96 <sungjin5891@gmail.com>
2021-09-28 19:26:21 +0900

    추가 : WebSocket Config:

---
+++ backend/module-stomp/src/main/java/com/cookking/stomp/StompApplication.java
@@ -0,0 +1,13 @@
+package com.cookking.stomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StompApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StompApplication.class, args);
+    }
+
+}
---
+++ backend/module-stomp/src/main/java/com/cookking/stomp/configs/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package com.cookking.stomp.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * Created by marathoner on 2021/09/28
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat").setAllowedOrigins("*").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app").enableSimpleBroker("/topic");
+    }
+}
