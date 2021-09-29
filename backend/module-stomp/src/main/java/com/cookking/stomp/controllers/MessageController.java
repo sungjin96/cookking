50bfadeae9dd44be3523e0514b2fc93f79e42a82
sungjin96 <sungjin5891@gmail.com>
2021-09-29 21:01:29 +0900

    c채팅 예제 코드 작성

--- backend/module-stomp/src/main/java/com/cookking/stomp/controllers/MessageController.java
+++ backend/module-stomp/src/main/java/com/cookking/stomp/controllers/MessageController.java
@@ -1,4 +1,28 @@
-package com.cookking.stomp.controllers;/**
+package com.cookking.stomp.controllers;
+
+import com.cookking.stomp.models.MessageDto;
+import com.cookking.stomp.storage.UserStorage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
  * Created by marathoner on 2021/09/29
- */public class MessageController {
+ */
+@RestController
+public class MessageController {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/chat/{to}")
+    public void sendMessage(@DestinationVariable String to, MessageDto messageDto) {
+        System.out.println("handling send message: " + messageDto + " to: " + to);
+        boolean isExists = UserStorage.getInstance().getUsers().contains(to);
+        if (isExists) {
+            simpMessagingTemplate.convertAndSend("/topic/messages/" + to, messageDto);
+        }
+    }
 }
